6bfb394a93c8288df91aceeca322a6363238a81a
Andrew Zhukov <oxyblade@gmail.com>
2018-08-02 18:31:41 +0300

    done function for the button, many changes html and css

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Music Store';
+  ngOnInit() {
+    const click = document.getElementById('btn');
+    click.addEventListener('click', function () {
+      alert('DONE');
+    });
+  }
 }
+
