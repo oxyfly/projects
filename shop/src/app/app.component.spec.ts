5378360e3ddc7d64f453821793fec742b68ffe21
Andrew Zhukov <oxyblade@gmail.com>
2018-08-02 16:32:05 +0300

    done UI home page: changed and add code html, css

--- shop/src/app/app.component.spec.ts
+++ shop/src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the store!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the music store!');
   }));
 });
