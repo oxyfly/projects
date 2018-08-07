0f1734248f0ff7824f4064652612988c9e69d725
Andrew Zhukov <oxyblade@gmail.com>
2018-08-07 13:04:27 +0300

    add and change code html, css, js

--- landing_page_002/js/scripts.js
+++ landing_page_002/js/scripts.js
@@ -4,3 +4,25 @@
 $('.carousel').carousel({
   interval: 0
 });
+
+/*  TO THE TOP BUTTON */
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.footer').fadeIn();
+    } else {
+      $('.footer').fadeOut();
+    }
+  });
+
+  $('.footer').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+});
+
+/*  SUBMIT BUTTON */
+function button() {
+  alert('PUSHED');
+}
