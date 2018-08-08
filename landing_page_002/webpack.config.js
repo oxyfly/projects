f782526c0b4394791444805e838739b285f82ef8
Andrew Zhukov <oxyblade@gmail.com>
2018-08-08 11:57:39 +0300

    add webpack.config.js

---
+++ landing_page_002/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './js/scripts.js',
+  ],
+  output: {
+    filename: './js/bundle.js'
+  },
+  devtool: "source-map",
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'js'),
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: 'env'
+        }
+      }
+    },
+    ]
+  },
+  plugins: [
+  ]
+};
