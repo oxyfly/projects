67c77f28e5851d786e0cbb909d4b9fa85ba12d05
Andrew Zhukov <oxyblade@gmail.com>
2018-08-16 13:11:20 +0300

    add new project: personal_page_animated_002; some other changes

---
+++ personal_page/personal_page_animated_002/js/scripts.js
@@ -0,0 +1,39 @@
+'use strict';
+
+$(document).ready(function() {
+  let current_delay = 500;
+  $("#title").css("opacity", 0).animate({
+    opacity: 1
+  }, 1000);
+  $("#menu").css("opacity", 0).delay(current_delay).animate({
+    opacity: 1
+  }, 1000);
+  $(".icons li").delay(current_delay).each(function () {
+    current_delay += 100;
+    console.log(current_delay);
+    $(this).css({
+      opacity: 0,
+      top: "-20px"
+    }).delay(current_delay).animate({
+      opacity: 1,
+      top: 0
+    }, 100);
+  });
+  current_delay += 800;
+  $("#footer").delay(current_delay).css("opacity", 0).animate({
+    opacity: 1
+  }, 1000);
+  $("#rights").delay(current_delay).css("opacity", 0).animate({
+    opacity: 1
+  }, 3000);
+  particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles-js config loaded');
+  });
+});
+
+document.ondragstart = noSelect;
+document.oncontextmenu = noSelect;
+document.onselectstart = noSelect;
+function noSelect() {
+  return false
+}
