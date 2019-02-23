4d8bad45c456103128be4fa9d61769fbbe8be19a
Andrew Zhukov <oxyblade@gmail.com>
2019-02-23 21:49:56 +0000

    added font and preloader

--- personal_page/personal_page_002_animated/js/scripts.js
+++ personal_page/personal_page_002_animated/js/scripts.js
@@ -1,16 +1,16 @@
-'use strict';
+// 'use strict';
 
 $(document).ready(function () {
-  $('body').hide().fadeIn(3000);
-  $('#particles-js').hide().fadeIn(3000);
+  'use strict';
+  // $('body').hide().fadeIn(3000);
 
-  let current_delay = 1000;
+  let current_delay = 3000;
   $("#title").css("opacity", 0).animate({
     opacity: 1
-  }, 1000);
+  }, 10000);
   $("#menu").css("opacity", 0).delay(current_delay).animate({
     opacity: 1
-  }, 1000);
+  }, 4000);
   $(".icons li").delay(current_delay).each(function () {
     current_delay += 100;
     console.log(current_delay);
@@ -22,13 +22,13 @@ $(document).ready(function () {
       top: 0
     }, 0);
   });
-  current_delay += 800;
+  current_delay += 3000;
   $("#footer").delay(current_delay).css("opacity", 0).animate({
     opacity: 1
-  }, 3000);
+  }, 4000);
   $("#rights").delay(current_delay).css("opacity", 0).animate({
     opacity: 1
-  }, 3000);
+  }, 5000);
   particlesJS.load('particles-js', 'particles.json', function () {
     console.log('callback - particles-js config loaded');
     setTimeout(function () {
@@ -37,6 +37,15 @@ $(document).ready(function () {
   });
 });
 
+// preloader
+jQuery(window).on('load', function () {
+  $preloader = $('.pre-loader');
+  $loader = $preloader.find('.pre-loader');
+  $loader.fadeOut();
+  $preloader.delay(2000).fadeOut('slow');
+});
+
+// forbiddance
 document.ondragstart = noSelect;
 document.oncontextmenu = noSelect;
 document.onselectstart = noSelect;
