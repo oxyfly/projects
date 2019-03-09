234543d160975dad3d432f16588e92ccea669653
Andrew Zhukov <oxyblade@gmail.com>
2019-03-09 21:00:50 +0000

    js changes

--- personal_page/personal_page_002_animated/js/nav-page-skills.js
+++ personal_page/personal_page_002_animated/js/nav-page-skills.js
@@ -3,23 +3,23 @@ $('.skills').hide().delay(2500).fadeIn(3000);
 $('.wrapper-skills-grid').hide().delay(4000).fadeIn(3000);
 
 $('.skill-01').hide().delay(5500).fadeIn(3000);
-$('.skill-02').hide().delay(6500).fadeIn(3000);
-$('.skill-03').hide().delay(7500).fadeIn(3000);
-$('.skill-04').hide().delay(8500).fadeIn(3000);
-$('.skill-05').hide().delay(9500).fadeIn(3000);
-$('.skill-06').hide().delay(10500).fadeIn(3000);
-$('.skill-07').hide().delay(11500).fadeIn(3000);
-$('.skill-08').hide().delay(12500).fadeIn(3000);
-$('.skill-09').hide().delay(13500).fadeIn(3000);
-$('.skill-10').hide().delay(14500).fadeIn(3000);
-$('.skill-11').hide().delay(15500).fadeIn(3000);
-$('.skill-12').hide().delay(16500).fadeIn(3000);
-$('.skill-13').hide().delay(17500).fadeIn(3000);
-$('.skill-14').hide().delay(18500).fadeIn(3000);
-$('.skill-15').hide().delay(19500).fadeIn(3000);
-$('.skill-16').hide().delay(20500).fadeIn(3000);
-$('.skill-17').hide().delay(21500).fadeIn(3000);
-$('.skill-18').hide().delay(22500).fadeIn(3000);
+$('.skill-02').hide().delay(5800).fadeIn(3000);
+$('.skill-03').hide().delay(6100).fadeIn(3000);
+$('.skill-04').hide().delay(6400).fadeIn(3000);
+$('.skill-05').hide().delay(6700).fadeIn(3000);
+$('.skill-06').hide().delay(7000).fadeIn(3000);
+$('.skill-07').hide().delay(7300).fadeIn(3000);
+$('.skill-08').hide().delay(7600).fadeIn(3000);
+$('.skill-09').hide().delay(7900).fadeIn(3000);
+$('.skill-10').hide().delay(8200).fadeIn(3000);
+$('.skill-11').hide().delay(8500).fadeIn(3000);
+$('.skill-12').hide().delay(8800).fadeIn(3000);
+$('.skill-13').hide().delay(9100).fadeIn(3000);
+$('.skill-14').hide().delay(9400).fadeIn(3000);
+$('.skill-15').hide().delay(9700).fadeIn(3000);
+$('.skill-16').hide().delay(10000).fadeIn(3000);
+$('.skill-17').hide().delay(10300).fadeIn(3000);
+$('.skill-18').hide().delay(10600).fadeIn(3000);
 
 // preloader
 jQuery(window).on('load', function () {
